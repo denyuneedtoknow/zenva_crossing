4e609458bc665a3ee4c6804c9b7b5c0d15b492d4
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2022-12-08 22:42:36 +0200

    add group

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ gameScene.preload = function () {
     this.load.image('background', 'assets/images/background.png')
     this.load.image('player', 'assets/images/player.png')
     this.load.image('enemy', 'assets/images/enemy.png')
+    this.load.image('treasure', 'assets/images/treasure.png')
     this.load.audio('start', 'assets/sounds/cat_start.mp3')
 }
 gameScene.init = function () {
@@ -13,21 +14,53 @@ gameScene.create = function () {
     this.startingCat = this.sound.add('start')
     this.startingCat.play()
     this.bg = this.add.sprite(0, 0, 'background')
-    this.player = this.add.sprite(0, 0, 'player').setPosition(50, 180).setScale(1)
-    this.enemy = this.add.sprite(0, 0, 'enemy').setPosition(350, 180).setScale(1.5)
+    // this.player = this.add.sprite(0, 0, 'player').setPosition(50, 180).setScale(1)
+    // this.enemy = this.add.sprite(0, 0, 'enemy').setPosition(350, 180).setScale(1.5)
     // bg.setOrigin(0, 0)
     let gameWidth = this.sys.game.config.width
     let gameHeight = this.sys.game.config.height
     this.bg.setPosition(gameWidth / 2, gameHeight / 2)
-
+    let cat = {
+        key: 'player',
+        setXY: {
+            x: 50,
+            y: 180
+        },
+        depth: 1,
+    }
+    let bomb = {
+        key: 'enemy',
+        setXY: {
+            x: 350,
+            y: 180
+        },
+        depth: 1,
+        scale: 1.5,
+    }
+    let star = {
+        key: 'treasure',
+        setXY: {
+            x: 600,
+            y: 180
+        },
+        depth: 1,
+        scale: 1.5,
+    }
+    let itemArray = [cat, bomb, star]
+    this.items = this.add.group(itemArray)
+    Phaser.Actions.Call(this.items.getChildren(), function (item) {
+        item.setInteractive().on('pointerdown', function () {
+            console.log(item.texture.key);
+        })
+    }, this)
 
 
 }
 gameScene.update = function () {
-    this.enemy.angle += 1
-    if (this.enemy.scale <= 2) {
-        this.enemy.scale += 0.01
-    }
+    // this.enemy.angle += 1
+    // if (this.enemy.scale <= 2) {
+    //     this.enemy.scale += 0.01
+    // }
 }
 let config = {
     type: Phaser.AUTO,
