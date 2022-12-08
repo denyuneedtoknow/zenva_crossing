10cad23139f6f873550d9540e1a870f3879e3e84
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2022-12-08 21:45:40 +0200

    sound

--- js/game.js
+++ js/game.js
@@ -1,17 +1,27 @@
 let gameScene = new Phaser.Scene('Game')
 gameScene.preload = function () {
-    this.load.image('background', 'assets/background.png')
-    this.load.image('player', 'assets/player.png')
-    this.load.image('enemy', 'assets/enemy.png')
+    this.load.image('background', 'assets/images/background.png')
+    this.load.image('player', 'assets/images/player.png')
+    this.load.image('enemy', 'assets/images/enemy.png')
+    this.load.audio('start', 'assets/sounds/cat_start.mp3')
 }
+gameScene.init = function () {
+
+}
+
 gameScene.create = function () {
+    this.startingCat = this.sound.add('start')
+    this.startingCat.play()
     this.bg = this.add.sprite(0, 0, 'background')
+    this.player = this.add.sprite(0, 0, 'player').setPosition(50, 180).setScale(1)
+    this.enemy = this.add.sprite(0, 0, 'enemy').setPosition(350, 180).setScale(1.5)
     // bg.setOrigin(0, 0)
     let gameWidth = this.sys.game.config.width
     let gameHeight = this.sys.game.config.height
     this.bg.setPosition(gameWidth / 2, gameHeight / 2)
-    this.player = this.add.sprite(0, 0, 'player').setPosition(50, 180).setScale(1)
-    this.enemy = this.add.sprite(0, 0, 'enemy').setPosition(350, 180).setScale(1.5)
+
+
+
 }
 gameScene.update = function () {
     this.enemy.angle += 1
@@ -23,6 +33,8 @@ let config = {
     type: Phaser.AUTO,
     width: 640,
     height: 360,
-    scene: gameScene
+    scene: gameScene,
+    pixelArt: false,
+    title: 'Crossing'
 }
 let game = new Phaser.Game(config)
